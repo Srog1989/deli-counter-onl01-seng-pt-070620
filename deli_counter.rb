cbe2f980d669e40b6579bdacc93504f2515109ca
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-13 18:38:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,9 @@
 def line(name)
   place_in_line=[]
   if name.length==0
-  
+   puts "The line is currently empty."
+  else
+    name_each_with_index do |item, index|
+    place_in_line<< "#{index+1}. #{item}"
+  end
+end

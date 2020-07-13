400be0427f477f5b783b5bde954e9e175d8de1fb
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-13 19:34:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(name)
-    place_in_line=[]
-    if name.length==0
-     puts "The line is currently empty."
+  place_in_line=[]
+  if name.length==0
+    puts "The line is currently empty."
    else 
      name.each_with_index do |item, index|
      place_in_line<< "#{index+1}. #{item}"

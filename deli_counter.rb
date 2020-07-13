3f5a5d82bc1a9d485c635febb196ab7b873142b9
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-13 19:09:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(name)
      puts "The line is currently empty."
    else
      name.each_with_index do |item, index|
-     place_in_line<< "#{index+1}, #{item}"
+     place_in_line<< "#{index+1}. #{item}"
    end
   puts "The line is currently: #{place_in_line.join (" ")}"
  end
